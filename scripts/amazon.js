5e143daa5e0475cf79b6b4552a169948779aa9b8
May Nyein Chan <mnc.mtht@gmail.com>
2026-02-05 14:30:57 +0630

    Added message for putting in the cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -2,6 +2,7 @@ import {cart, addToCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 
 let productsHTML = "";
+let addedMessageTimeouts = {};
 
 products.forEach((product) => {
     productsHTML += `
@@ -28,7 +29,7 @@ products.forEach((product) => {
           </div>
 
           <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
               <option selected value="1">1</option>
               <option value="2">2</option>
               <option value="3">3</option>
@@ -44,7 +45,7 @@ products.forEach((product) => {
 
           <div class="product-spacer"></div>
 
-          <div class="added-to-cart">
+          <div class="added-to-cart js-added-to-cart-${product.id}">
             <img src="images/icons/checkmark.png">
             Added
           </div>
@@ -69,10 +70,40 @@ function updateCartQuantity() {
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     button.addEventListener("click", () => {
-        const productId = button.dataset.productId;
+      const productId = button.dataset.productId;
 
-        addToCart(productId);
-        updateCartQuantity();
+      const userSelection = document.querySelector(
+        `.js-quantity-selector-${productId}`
+      );
+      const selectedNumber = Number(userSelection.value);
+
+      let matchingItem = cart.find(item => item.productId === productId);
+
+      if (matchingItem) {
+        matchingItem.quantity += selectedNumber;
+      } else {
+        cart.push({
+          productId,
+          quantity: selectedNumber
+        });
+      }
+
+      let cartQuantity = 0;
+      cart.forEach(item => cartQuantity += item.quantity);
+      document.querySelector('.js-cart-quantity').textContent = cartQuantity;
+
+      const addedMessage = document.querySelector(
+        `.js-added-to-cart-${productId}`
+      );
+
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) clearTimeout(previousTimeoutId);
+
+      addedMessage.classList.add('added-to-cart-visible');
+
+      addedMessageTimeouts[productId] = setTimeout(() => {
+        addedMessage.classList.remove('added-to-cart-visible');
+      }, 2000);
     });
 });
  

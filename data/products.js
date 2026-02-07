05014e64be9f51b833ff941927908a9660f159c7
May Nyein Chan <mnc.mtht@gmail.com>
2026-02-07 18:32:17 +0630

    Use inheritance and polymorphism to add size chart --amend

--- data/products.js
+++ data/products.js
@@ -40,18 +40,6 @@ class Product {
   }
 }
 
-const product1 = new Product({
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  });
-
 class Clothing extends Product {
   sizeChartLink;
 

bc69df64074a24dda4a2ba2d3abc0c0a5d9cf56c
May Nyein Chan <mnc.mtht@gmail.com>
2026-02-07 18:45:08 +0630

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -56,8 +56,37 @@ class Clothing extends Product {
       </a>
     `;
   }
-
 }
+
+/*
+  const date = new Date();
+  console.log(date);
+  console.log(date.toLocaleDateString());
+*/
+
+/*
+  console.log(this);
+  const object2 = {
+    a: 2,
+    b: this.a,
+  }
+*/
+
+/*
+function logThis() {
+  console.log(this);
+}
+logThis();
+logThis.call('Hello');
+
+this
+const object3 = {
+  method: () => {
+    console.log(this);
+  }
+};
+object3.method();
+*/
 
 export const products = [
   {

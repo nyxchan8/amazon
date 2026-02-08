881434e55989ff04216e23fcbd4a914dfaa41da0
May Nyein Chan <mnc.mtht@gmail.com>
2026-02-08 17:17:18 +0630

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -126,6 +126,8 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -153,6 +155,10 @@ export function loadProducts(fun) {
     console.log('load products');
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
